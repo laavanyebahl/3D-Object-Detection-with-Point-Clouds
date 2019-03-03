238133de20fa7f63eb4ecb87d23f94fe751f75f9
Laavanye Bahl <laavanye.bahl@gmail.com>
2019-03-02 20:12:36 -0500

    add file to tranform point cloud from camera link to world link

---
+++ 3Dperception_ws/src/object_detection/src/camera_to_world_point_cloud.cpp
@@ -0,0 +1,62 @@
+// Author: Laavanye Bahl
+
+#include <boost/random.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <ros/ros.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+
+/*
+* This node transforms point cloud from /camera_link frame to /world frame with noise
+*/
+
+class CloudTransformer
+{
+public:
+  explicit CloudTransformer(ros::NodeHandle nh)
+    : nh_(nh),
+      demo_(0)
+  {
+    // Define Publishers and Subscribers here
+    pcl_sub_ = nh_.subscribe("/camera/depth_registered/points", 1, &CloudTransformer::pclCallback, this);
+    pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/stereo_cam/world/points", 1);
+
+    buffer_.reset(new sensor_msgs::PointCloud2);
+    buffer_->header.frame_id = "world";
+  }
+
+private:
+  ros::NodeHandle nh_;
+  bool demo_;
+  ros::Subscriber pcl_sub_;
+  ros::Publisher pcl_pub_;
+  tf::TransformListener listener_;
+  sensor_msgs::PointCloud2::Ptr buffer_;
+
+  void pclCallback(const sensor_msgs::PointCloud2ConstPtr& pcl_msg)
+  {
+    float standard_deviation = 0.025;
+    listener_.waitForTransform("world", "camera_link", ros::Time::now(), ros::Duration(1.0));
+    pcl_ros::transformPointCloud("world", *pcl_msg, *buffer_, listener_);
+  }
+};  // End of class CloudTransformer
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "point_cloud_tf");
+  ros::NodeHandle nh;
+
+  CloudTransformer tranform_cloud(nh);
+
+  // Spin until ROS is shutdown
+  while (ros::ok())
+    ros::spin();
+
+  return 0;
+}
