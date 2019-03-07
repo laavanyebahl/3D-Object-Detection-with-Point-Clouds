c813f258e33aa9b4ea2196fc7dbc478aeef9670d
Laavanye Bahl <laavanye.bahl@gmail.com>
2019-03-06 22:36:37 -0500

    Header file for object structure

---
+++ 3Dperception_ws/src/object_detection/include/object_detection/object.h
@@ -0,0 +1,21 @@
+#ifndef _OBJECT_DETECTION_OBJECT_H_
+#define _OBJECT_DETECTION_OBJECT_H_
+
+#include <string>
+
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Vector3.h"
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
+
+namespace object_detection {
+struct Object {
+  std::string name;
+  double confidence;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
+  geometry_msgs::Pose pose;
+  geometry_msgs::Vector3 dimensions;
+};
+}  // namespace object_detection
+
+#endif  // _OBJECT_DETECTION_OBJECT_H_
